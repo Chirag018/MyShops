1a8ca92101d4d669745709e314008c1b23edf1f0
Chirag018 <chiragmmehta16@gmail.com>
2021-03-03 22:44:51 +0530

    merged finally
    merged finally.

--- lib/provider/products.dart
+++ lib/provider/products.dart
@@ -92,19 +92,19 @@ class Products with ChangeNotifier {
 // <<<<<<< Updated upstream
 // =======
 
-  void updateProduct(String id, Product newProduct) {
-    final prodIndex = _items.indexWhere((prod) => prod.id == id);
-    if (prodIndex >= 0) {
-      _items[prodIndex] = newProduct;
-      notifyListeners();
-    } else {
-      print('........');
-    }
-  }
+  // void updateProduct(String id, Product newProduct) {
+  //   final prodIndex = _items.indexWhere((prod) => prod.id == id);
+  //   if (prodIndex >= 0) {
+  //     _items[prodIndex] = newProduct;
+  //     notifyListeners();
+  //   } else {
+  //     print('........');
+  //   }
+  // }
 
-  void deleteProducts(String id) {
-    _items.removeWhere((prod) => prod.id == id);
-    notifyListeners();
-  }
+  // void deleteProducts(String id) {
+  //   _items.removeWhere((prod) => prod.id == id);
+  //   notifyListeners();
+  // }
 // >>>>>>> Stashed changes
 }
